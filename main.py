679f49459798ae1efbcce1597cecacf6c2176275
TimTen10 <Tim.Eckle94@gmail.com>
2020-08-27 15:26:39 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# In this project I will implement NEAT (Neuroevolution of Augmenting Topologies) and use it to
+# do Reinforcement Learning on ___.
+# The original paper, this work is based on, can be found at: http://nn.cs.utexas.edu/downloads/papers/stanley.ec02.pdf
+
+# A Genome (Genotype) consists of Node Genes and Connection Genes. Node Genes contain the Node ID / number and the type
+# of the node (Input, Hidden or Output). Connection Genes contain the Node ID of the starting and ending Node of the
+# connection, the weight, whether or not the connection is active in the current Genome and lastly the historic marking.
+# The build Neural Network acts as the Phenotype.
+
+# There are several types of mutation: Add Connection, Add Node and change weight.
+
+# The Network will start as small as possible and will expand only through evolution.
